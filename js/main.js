8eba211d17d0fa01b94ceee49c75da3ddf75df59
Oksana Banshchykova <88445174+oxica@users.noreply.github.com>
2024-09-13 12:45:52 +0200

    home css js

--- js/main.js
+++ js/main.js
@@ -26,3 +26,13 @@ const linkAction = () =>{
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
